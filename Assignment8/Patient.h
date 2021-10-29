f585d031da2581d11ee07e8207cbcd866e9fdcb0
NhanThai0501 <72413541+NhanThai0501@users.noreply.github.com>
2021-10-28 22:28:22 -0700

    Update Patient.h

--- Assignment8/Patient.h
+++ Assignment8/Patient.h
@@ -14,6 +14,8 @@ private:
 	string careUnit;
 	static string ER_description[5];
 public:
+
+	// Default constructor
 	Patient()
 	{
 		checkedInTime;
@@ -23,51 +25,71 @@ public:
 		gender = 'u';
 	}
 
+	// Precondition: valid input
+	// Postcondition: assign value
 	void setCheckedInTime(unsigned int t)
 	{
 		this->checkedInTime= t;
 	}
 
+	// Precondition: NA
+	// Postcondition: return value
 	unsigned int getCheckedInTime() const
 	{
 		return checkedInTime;
 	}
 
+	// Precondition: valid input
+	// Postcondition: assign value
 	void setPriority(int priority)
 	{
 		this->priority = priority;
 	}
 
+	// Precondition: NA
+	// Postcondition: return value
 	int getPriority() const
 	{
 		return priority;
 	}
 
+	// Precondition: valid input
+	// Postcondition: assign value
 	void setName(string name)
 	{
 		this->name = name;
 	}
 
+	// Precondition: NA
+	// Postcondition: return value
 	string getName() const
 	{
 		return name;
 	}
 
+	// Precondition: valid input
+	// Postcondition: assign value
 	void setAge(int age)
 	{
 		this->age = age;
 	}
 
+	// Precondition: NA
+	// Postcondition: return value
 	int getAge() const
 	{
 		return age;
 	}
 
+	// Precondition: valid input
+	// Postcondition: assign value
 	void setGender(char gender)
 	{
 		this->gender = gender;
 	}
-
+	 
+	// Precondition: valid input
+	// Postcondition: assign value
 	char getGender() const
 	{
 		return gender;
@@ -78,6 +100,8 @@ public:
 	friend ostream& operator<<(ostream& outs, const Patient& obj);
 };
 
+// Precondition: valid input
+// Postcondition: return true if object 1 is smaller than object 2
 bool operator <(const Patient& P1, const Patient& P2)
 {
 	return P1.priority < P2.priority;
@@ -85,6 +109,8 @@ bool operator <(const Patient& P1, const Patient& P2)
 
 string Patient::ER_description[5] = { "Stable, with no resources anticipated or prescriptions", "Stable, with only one type of resource anticipated " ,"Stable, with multiple types of resources needed to investigate or treat", "High risk of deterioration, or signs of a time-critical problem", "Immediate, life-saving intervention required without delay" };
 
+// Precondition: valid input
+// Postcondition: overloading operator << 
 ostream& operator <<(ostream& outs, const Patient& obj)
 {
 	outs << "ER level: " << obj.getPriority() << " - " << obj.ER_description[obj.getPriority()-1] << '\n';
